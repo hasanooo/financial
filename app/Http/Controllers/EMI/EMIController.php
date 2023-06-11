22866267d3f3a040b229e5c019f1522e8a08da13
UnrealBrain <faijulabedin7@gmail.com>
2023-06-11 16:07:36 +0600

    emi submit

--- app/Http/Controllers/EMI/EMIController.php
+++ app/Http/Controllers/EMI/EMIController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\EMI;
 use App\Models\Product;
+use App\Models\SellingPayment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -40,32 +41,36 @@ class EMIController extends Controller
         $sale = new EMI();
         $random_number = rand(1, 1000);
         $random_string = Str::random(4);
-        $sale->invoice = "bs" . $random_number . $random_string;
+        $sale->invoice = "bsemi" . $random_number . $random_string;
         // $sale->service_name=$request->service_name;
-        $sale->total_amount = $request->total;
-        $sale->discount_amount = $request->discount_amount;
+        $sale->customer_id = $request->customer;
+        $sale->product_id = $request->product;
+        $sale->date = $request->date;
         // $sale->earning=$request->earning;
-        $sale->service_id = $request->service_id;
-        $sale->assistant_id = $request->assistant_id;
-        $sale->hospital_customer_id = $request->hospital_customer_id;
-        $sale->earning = $request->earning;
-        $sale->user_type = 'assistant';
+        $sale->discount = $request->discount;
 
+        $sale->total_price = $request->payable_amount;
+        $sale->paid_amount  = $request->receive_amount;
+        $sale->emi_rate = $request->emi_rate;
+        $sale->duration = $request->duration;
+        $sale->emi_quantity = $request->emi_quantity;
+        $sale->emi_amount = $request->emi_amount;
+        $sale->type = 'cash';
         $sale->save();
 
-        // $payment = new SellingPayment();
-        // $payment->service_sale_id = $sale->id;
-        // $payment->payment_method = 'cash';
-        // $payment->payment_note = $request->payment_note;
-        // $payment->amount_paid = $request->amount_paid;
-        // $payment->save();
+        $payment = new SellingPayment();
+        $payment->e_m_i_id = $sale->id;
+        $payment->payment_method = 'cash';
+        $payment->payment_note = 'Ok';
+        $payment->amount_paid = $request->receive_amount;
+        $payment->save();
 
         // $c_history = new CustomerPaymentHistory();
         // $c_history->hospital_customer_id=$request->hospital_customer_id; 
         // $c_history->amount=$request->amount_paid;
         // $c_history->save();
 
-        return redirect()->route('sales.report');
+        return redirect()->route('emi.index');
         
     }
 }
