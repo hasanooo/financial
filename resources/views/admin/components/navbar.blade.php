33834f28407fb8e1103afbc9f77820345904f797
imon213 <emonf666@gmail.com>
2023-06-13 17:11:57 +0600

    logout at navbar

--- resources/views/admin/components/navbar.blade.php
+++ resources/views/admin/components/navbar.blade.php
@@ -43,33 +43,11 @@
 
         <!-- Messages Dropdown Menu -->
         <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge"></span>
+            <a class="nav-link"  href="{{route('logout')}}">
+               
+                <span class="badge badge-danger navbar-badge">Logout</span>
             </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="#" class="dropdown-item">
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="" alt="" class="img-size-50 img-circle mr-3">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-
-                                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm"></p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i></p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-            </div>
+           
         </li>
         <!-- Notifications Dropdown Menu -->
         <li class="nav-item dropdown">
