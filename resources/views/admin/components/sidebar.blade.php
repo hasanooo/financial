d56324c91488b1a96aa25a4ed5c4b342fefd9290
UnrealBrain <faijulabedin7@gmail.com>
2023-08-20 18:59:04 +0600

    Logo

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -2,10 +2,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link" style="text-decoration: none;">
-        <img src="{{ asset('Admin/dist/img/AdminLogo.jpg') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+        {{-- <img src="{{ asset('Admin/dist/img/AdminLogo.jpg') }}" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
         <span class="brand-text font-weight-bold">Bengal Software</span>
-        <i class="fas fa-xmark text-primary"></i>
+        {{-- <i class="fas fa-xmark text-primary"></i> --}}
     </a>
 
     
